caac01b48e11010cc38ab1532ac12f2bfe47871f
AronGomu <neverismine@gmail.com>
2026-08-19 13:52:31 +0200

    feat(hm): add gimp for image editing

--- home/aron/packages.nix
+++ home/aron/packages.nix
@@ -301,6 +301,7 @@ in
       sushi # Nautilus Space quick preview (org.gnome.NautilusPreviewer)
       file-roller
       flameshot
+      gimp
       kdePackages.kdenlive
       keepassxc
       mediainfo
