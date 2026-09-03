3b049e79832bf86a56532f4af80726fa07e48209
AronGomu <neverismine@gmail.com>
2026-09-03 23:59:25 +0200

    fix(hm): stop Redshift failure under Wayland

--- home/aron/desktop.nix
+++ home/aron/desktop.nix
@@ -20,23 +20,6 @@ let
   '';
 in
 {
-  # Hardest blue-light cut: night floor 1000K (redshift min).
-  services.redshift = {
-    enable = true;
-    provider = "manual";
-    latitude = "48.86";
-    longitude = "2.35";
-    temperature = {
-      day = 4500;
-      night = 1000;
-    };
-    settings.redshift = {
-      brightness-day = "1.0";
-      brightness-night = "0.7";
-    };
-    tray = true;
-  };
-
   gtk = {
     enable = true;
     font = {
